661b0e9d07f06340e2c9a02547218cc2852a408a
AdamSimkinbgu <simkinad@post.bgu.ac.il>
2024-07-31 15:40:14 +0300

    LoginVM implemented
    notifiable required and backend controller required to be implemented.

--- Frontend/ViewModel/LoginVM.cs
+++ Frontend/ViewModel/LoginVM.cs
@@ -1,12 +1,83 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Windows;
+using Frontend.Model;
 
-namespace Frontend.ViewModel
+namespace IntroSE.Kanban.Frontend.ViewModel
 {
-    internal class LoginVM
+    internal class LoginVM : Notifiable
     {
+        private BackendController controller;
+
+        public LoginVM(BackendController controller)
+        {
+            this.controller = controller;
+        }
+
+        public LoginVM()
+        {
+            controller = new BackendController();
+        }
+
+        private string errorMessage = "";
+
+        public string ErrorMessage
+        {
+            get => errorMessage;
+            set
+            {
+                errorMessage = value;
+                RaisePropertyChanged("ErrorMessage");
+            }
+        }
+
+        private string email = "";
+
+        public string Email
+        {
+            get => email;
+            set
+            {
+                email = value;
+                FieldsAreNotEmpty = string.IsNullOrWhiteSpace(value);
+            }
+        }
+
+        private string password = "";
+
+        public string Password
+        {
+            get => password;
+            set
+            {
+                password = value;
+                FieldsAreNotEmpty = string.IsNullOrWhiteSpace(value);
+            }
+        }
+
+        internal bool FieldsAreNotEmpty
+        {
+            get => !(string.IsNullOrWhiteSpace(email) && string.IsNullOrWhiteSpace(password));
+            set => RaisePropertyChanged("FieldsAreNotEmpty");
+        }
+
+        internal UserModel? Login()
+        {
+            Tuple<UserModel?, string> t = controller.Login(email, password);
+            ErrorMessage = t.Item2;
+            return t.Item1;
+        }
+
+        internal UserModel? Register()
+        {
+            Tuple<UserModel?, string> t = controller.Register(email, password);
+            ErrorMessage = t.Item2;
+            return t.Item1;
+        }
+
+        internal List<string> GetUserBoards()
+        {
+            return controller.GetUserBoards(Email);
+        }
     }
 }
