49a85cdab894c1fbc53211a946c3f69f9e05269c
AdamSimkinbgu <simkinad@post.bgu.ac.il>
2024-07-31 15:14:03 +0300

    creating dev and taskmodel imple
    missing xaml for frontend was implemented, added my taskmodel also to do things more quickly

---
+++ Frontend/Model/TaskModel.cs
@@ -0,0 +1,14 @@
+﻿namespace IntroSE.Kanban.Backend.ServiceLayer
+{
+    internal class TaskModel
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+        public TaskModel(TaskSL task)
+        {
+            this.Title = task.Title;
+            this.Description = task.Description;
+        }
+    }
+}
