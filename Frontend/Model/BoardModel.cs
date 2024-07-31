10f63d9ae3bfd7bdee0519282eb42bf6a0da238f
Naim Elijah <elijahn@post.bgu.ac.il>
2024-07-31 15:56:48 +0300

    added boardview and Board Model

--- Frontend/Model/BoardModel.cs
+++ Frontend/Model/BoardModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Frontend.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,65 @@ using System.Threading.Tasks;
 
 namespace Frontend.Model
 {
-    internal class BoardModel
+    public class BoardModel : NotifiableModel
     {
+        private string name;
+        public string Name
+        {
+            get => name;
+            set
+            {
+                name = value;
+                RaisePropertyChanged("Name");
+            }
+        }
+
+        private List<string> backlogTasks;
+        public List<string> BacklogTasks
+        {
+            get => backlogTasks;
+            set
+            {
+                backlogTasks = value;
+                RaisePropertyChanged("BacklogTasks");
+            }
+        }
+
+        private List<string> inProgressTasks;
+        public List<string> InProgressTasks
+        {
+            get => inProgressTasks;
+            set
+            {
+                inProgressTasks = value;
+                RaisePropertyChanged("InProgressTasks");
+            }
+        }
+
+        private List<string> doneTasks;
+        public List<string> DoneTasks
+        {
+            get => doneTasks;
+            set
+            {
+                doneTasks = value;
+                RaisePropertyChanged("DoneTasks");
+            }
+        }
+
+        private UserModel user;
+        public UserModel User
+        {
+            get => user;
+        }
+
+        public BoardModel(BackendController controller, UserModel user, string name) : base(controller)
+        {
+            this.name = name;
+            this.user = user;
+            backlogTasks = new List<string>();
+            inProgressTasks = new List<string>();
+            doneTasks = new List<string>();
+        }
     }
 }
--- Frontend/View/BoardView.xaml.cs
+++ Frontend/View/BoardView.xaml.cs
@@ -1,12 +1,37 @@
-﻿using System;
+﻿using Frontend.Model;
+using Frontend.View;
+using Frontend.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace Frontend.View
 {
-    internal class BoardView
+    public partial class BoardView : Window
     {
+        BoardVM vm;
+        BoardModel model;
+
+        public BoardView(BoardModel board)
+        {
+            InitializeComponent();
+            vm = new BoardVM(board);
+            DataContext = vm;
+            model = board;
+            Title = board.Name;
+            BacklogList.ItemsSource = vm.Backlog;
+            InProgressList.ItemsSource = vm.InProgress;
+            DoneList.ItemsSource = vm.Done;
+        }
+
+        private void BackButton_Click(object sender, RoutedEventArgs e)
+        {
+            UserView userWindow = new UserView(model.User);
+            userWindow.Show();
+            Close();
+        }
     }
 }
